87cacf60b8b52f37bdd66c3e46adaf6065811fd3
Jessy Adam <80044343+JessyEstTropCool@users.noreply.github.com>
2022-10-20 14:46:47 +0200

    autoscroll 1

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const hamburgerButton = document.getElementById('hamburger');
 const navList = document.getElementById('nav-list');
 const skills = document.getElementsByClassName('skills');
 
+const autoScrollDelay = 60;
+
 function toggleButton()
 {
     navList.classList.toggle('show');
@@ -15,12 +17,57 @@ navList.childNodes.forEach((child, key, parent) => {
     });
 });
 
+for (let index = 0; index < skills.length; index++) 
+{
+    const element = skills[index];
+    if ( element.scrollWidth > element.offsetWidth ) element.setAttribute("way", "right");
+    element.setAttribute("time", "0");
+
+    element.addEventListener('mouseenter', () => {
+        element.setAttribute("wait", "true");
+    });
+    element.addEventListener('touchstart', () => {
+        element.setAttribute("wait", "true");
+    });
+    element.addEventListener('mouseleave', () => {
+        element.removeAttribute("wait");
+    });
+    element.addEventListener('touchend', () => {
+        element.removeAttribute("wait");
+    });
+    
+    console.info(element);
+}
+
 setInterval(() => {
 
     for (let index = 0; index < skills.length; index++) 
     {
         const element = skills[index];
-        element.scrollLeft += 1;
+
+        if ( element.scrollWidth <= element.offsetWidth ) element.removeAttribute("way");
+        else 
+        {
+            if ( element.getAttribute("wait") === null )
+            {
+                if ( element.scrollLeft == 0 || element.scrollLeft == element.scrollLeftMax )
+                    element.setAttribute("time", Number(element.getAttribute("time")) + 1 );
+    
+                if ( element.getAttribute("way") === null || ( element.scrollLeft <= 0 && element.getAttribute("time") >= autoScrollDelay ) ) 
+                {
+                    element.setAttribute("way", "right");
+                    element.setAttribute("time", "0");
+                }
+                else if ( element.scrollLeft >= element.scrollLeftMax && element.getAttribute("time") >= autoScrollDelay ) 
+                {
+                    element.setAttribute("way", "left");
+                    element.setAttribute("time", "0");
+                }
+                
+                if ( element.getAttribute("way") === "right" ) element.scrollLeft += 1;
+                else element.scrollLeft -= 1;
+            }
+        }
     }
 
 }, 1);
