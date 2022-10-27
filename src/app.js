31c0bdb55e26779603ccc0503b32610007fde635
JessyEstTropCool <80044343+JessyEstTropCool@users.noreply.github.com>
2022-10-27 17:51:49 +0200

    Scroll fini assez bien

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ const hamburgerButton = document.getElementById('hamburger');
 const navList = document.getElementById('nav-list');
 const skills = document.getElementsByClassName('skills');
 
-const autoScrollDelay = 60;
+const autoScrollDelay = 1000;
+const scrollRate = 0.1;
+var lastUpdate = Date.now();
 
 function toggleButton()
 {
@@ -23,42 +25,48 @@ for (let index = 0; index < skills.length; index++)
     if ( element.scrollWidth > element.offsetWidth ) element.setAttribute("way", "right");
     element.setAttribute("time", "0");
 
-    element.addEventListener('mouseenter', () => {
-        console.log('mouseenter');
-        element.setAttribute("wait", "true");
-    });
-    element.addEventListener('active', () => {
-        console.log('active');
-        element.setAttribute("wait", "true");
-    });
-    element.addEventListener('mouseleave', () => {
-        console.log('mouseleave');
-        element.removeAttribute("wait");
-    });
+    element.addEventListener('mouseenter', () => element.setAttribute("wait", "true"));
+    element.addEventListener('touchstart', () => {element.setAttribute("wait", "true")});
+    element.addEventListener('touchmove', () => element.setAttribute("wait", "true"));
+    
+    element.addEventListener('mouseleave', () => element.removeAttribute("wait"));
+    element.addEventListener('touchend', () => element.removeAttribute("wait"));
     
     console.info(element);
 }
 
-setInterval(() => {
+//var skTitle = document.getElementById('skills-title');
+
+var scrollInterval = setInterval(() => {
+    var now = Date.now();
+    var dt = now - lastUpdate;
+    lastUpdate = now;
+
+    //skTitle.innerText = dt + "ms";
 
     for (let index = 0; index < skills.length; index++) 
     {
         const element = skills[index];
 
-        if ( element.scrollWidth <= element.offsetWidth ) element.removeAttribute("way");
+        if ( element.scrollWidth <= element.offsetWidth ) 
+        {
+            element.removeAttribute("way");
+            element.parentElement.classList.remove("skills-shadow");
+        }
         else 
         {
+            element.parentElement.classList.add("skills-shadow");
             if ( element.getAttribute("wait") === null )
             {
-                if ( element.scrollLeft == 0 || element.scrollLeft == element.scrollLeftMax )
-                    element.setAttribute("time", Number(element.getAttribute("time")) + 1 );
+                if ( element.scrollLeft == 0 || element.scrollLeft == getScrollLeftMax(element) )
+                    element.setAttribute("time", Number(element.getAttribute("time")) + dt );
     
                 if ( element.getAttribute("way") === null || ( element.scrollLeft <= 0 && element.getAttribute("time") >= autoScrollDelay ) ) 
                 {
                     element.setAttribute("way", "right");
                     element.setAttribute("time", "0");
                 }
-                else if ( element.scrollLeft >= element.scrollLeftMax && element.getAttribute("time") >= autoScrollDelay ) 
+                else if ( element.scrollLeft >= getScrollLeftMax(element) && element.getAttribute("time") >= autoScrollDelay ) 
                 {
                     element.setAttribute("way", "left");
                     element.setAttribute("time", "0");
@@ -71,3 +79,9 @@ setInterval(() => {
     }
 
 }, 1);
+
+function getScrollLeftMax(elem) 
+{
+    if (elem.scrollLeftMax === undefined) return elem.scrollWidth - elem.clientWidth;
+    else return elem.scrollLeftMax;
+}
